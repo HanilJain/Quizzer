369385ae8431619c9caf74895c5141f572b24b8b
HanilJain <shirshj0111@gmail.com>
2023-01-14 16:31:18 +0530

    change

--- Quizzer/settings.py
+++ Quizzer/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'Quiz.apps.QuizConfig',
     'User.apps.UserConfig',
     'crispy_forms',
-    'jquery'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
